60930cff247206d9932cd8c611799253f0cc5666
Kaan Ersoy <kaanersoy12@gmail.com>
2023-12-22 17:05:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const router = require("./router");
 // middlewares
 app.use(router);
 
+app.get("/", (_,res) => {
+  res.json({
+    "🦄": "emre"
+  })
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is listening on ${port}`);
